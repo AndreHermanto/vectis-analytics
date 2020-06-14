5ace4f96867e0066110aeec545b13662681662cc
Andre Hermanto <andre.hermanto93@gmail.com>
2020-06-14 21:11:26 +1000

    Remove sql and change db container name

--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     auth0ClientSecret: 'oMuRh3VyxlspqK6M84FmJ2XLa09PW6WsPvTCan2QB6f99OqUz2kJAokBTOjbtTEo',
-    dbHost: 'mysql',
+    dbHost: 'garvan_vectis_analytics_db',
     dbUser: 'root',
     dbPassword: 'password',
     dbDatabase: 'VECTIS_ANALYTICS',
