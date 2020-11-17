b7f2253a62c4a6a254241e81e1cd9a3a80d1ad16
Andre Hermanto <andre.hermanto93@gmail.com>
2020-11-18 02:24:39 +1100

    Unctrack config files

--- config/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    auth0ClientSecret: 'oMuRh3VyxlspqK6M84FmJ2XLa09PW6WsPvTCan2QB6f99OqUz2kJAokBTOjbtTEo',
-    dbHost: 'localhost',
-    dbUser: 'root',
-    dbPassword: 'password',
-    dbDatabase: 'VECTIS_ANALYTICS',
-};
